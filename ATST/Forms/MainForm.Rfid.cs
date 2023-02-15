27552a9df21bcc4e596769606d407137d0a88eb8
Sunwookim1 <tjsrkem05070@gmail.com>
2023-02-15 09:15:43 +0900

    marge

--- ATST/Forms/MainForm.Rfid.cs
+++ ATST/Forms/MainForm.Rfid.cs
@@ -114,7 +114,6 @@ namespace ATST.Forms
         }
 
         int currentPort = 0;
-        int SavePort = -1;
 
         private void AddTagItem(string epc,
                                 string rssi,
@@ -127,7 +126,7 @@ namespace ATST.Forms
                 item.SubItems[2].Text = port;
             }
             else
-            {/*
+            {
                 string[] items = new string[3];
                 items[0] = epc;
                 items[1] = rssi;
@@ -135,17 +134,13 @@ namespace ATST.Forms
                 item = new ListViewItem(items);
                 listview_rfid_inventory_tag_data.BeginUpdate();
                 listview_rfid_inventory_tag_data.Items.Add(item);
-                listview_rfid_inventory_tag_data.EndUpdate();*/
+                listview_rfid_inventory_tag_data.EndUpdate();
             }
 
             // 로직 구현
-            currentPort = Convert.ToInt32(port);
 
-            out_proccess(epc, port, rssi);
-
-            input_proccess(epc, port, rssi);
             // 첫번째 포트 저장
-            /*first_port_set(port);
+            first_port_set(port);
 
             // 포트 순회해서 상태 변환
             allport_inputstate_change(port);
@@ -154,23 +149,7 @@ namespace ATST.Forms
             output_proccess(epc, port, rssi);
 
             // 입고 처리
-            input_proccess(epc, port, rssi);*/
-
-            var EpcList = SharedValues.mTagSaveDictionary.Select(x => x.Value).ToList();
-
-            listview_rfid_inventory_tag_data.Items.Clear();
-            foreach (var value in EpcList)
-            {
-                string[] items = new string[3];
-                items[0] = value.Epc;
-                items[1] = value.Rssi.ToString();
-                items[2] = value.Port.ToString();
-                item = new ListViewItem(items);
-                listview_rfid_inventory_tag_data.BeginUpdate();
-                listview_rfid_inventory_tag_data.Items.Add(item);
-                listview_rfid_inventory_tag_data.EndUpdate();
-            }
-
+            input_proccess(epc, port, rssi);
         }
 
         private async void btn_rfid_connect_Click(object sender, EventArgs e)
@@ -181,7 +160,7 @@ namespace ATST.Forms
             {
                 if (SharedValues.ConnectionType == SharedValues.InterfaceType.SERIAL)
                 {
-                    await Reader.GetReaderAsync("COM11", 115200, 2).ConfigureAwait(true);
+                    await Reader.GetReaderAsync("COM11", 115200, 8).ConfigureAwait(true);
                     Log.WriteLine("INFO. Reader Setting ConnectionType({0}).", SharedValues.ConnectionType);
 
                 }
